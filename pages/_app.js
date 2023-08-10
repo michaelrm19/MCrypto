8e223abf14169668583205fc4f669af856abdc06
Michael Nguyen <mnguyen2000@gmail.com>
2023-08-10 02:42:59 -0700

    Added Navigation Bar, Footer, and Banner on home page

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,9 @@ const MyApp = ({ Component, pageProps }) => (
   <ThemeProvider attribute="class">
     <div className="dark:bg-nft-dark bg-white min-h-screen">
       <Navbar />
-      <Component {...pageProps} />
+      <div className="pt-65">
+        <Component {...pageProps} />
+      </div>
       <Footer />
     </div>
     <Script src="https://kit.fontawesome.com/8efe200e65.js" crossOrigin="anonymous" />
